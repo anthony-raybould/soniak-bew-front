cf7bf83580776e768997f290a5f5fa37b9e8a64a
Anthony Raybould <anthony.raybould@kainos.com>
2023-08-31 13:40:47 +0100

    Initialised /client endpoint.

--- controller/clientController.ts
+++ controller/clientController.ts
@@ -74,4 +74,24 @@ export const clientController = function(app : Application) {
 
         res.render("assign-client-project-success");
     });
+
+    app.get("/client", async (req : Request, res : Response) => {
+
+        try {
+
+            let clients : Client[];
+            clients = await getClients(req.session.token);
+
+            res.render("list-clients", {
+                clients : clients,
+            });
+
+        } catch (e) {
+            console.error(e);
+
+
+            res.locals.errormessage = "A problem occurred generating this page.";
+            res.render("list-clients");
+        }
+    });
 }
